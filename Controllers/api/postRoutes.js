7ce6d9ae97fc2a39854707c92c454bad35df1436
Dereje Ayele <derejeatomsa2@gmail.com>
2021-06-05 06:32:04 -0400

    minor clean up

--- Controllers/api/postRoutes.js
+++ Controllers/api/postRoutes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-// const sequelize = require('../../config/connection');
 const { Comment, Post, User} = require('../../models');
 const withAuth = require('../../utils/auth');
 
--- Controllers/homeRoutes.js
+++ Controllers/homeRoutes.js
@@ -1,6 +1,5 @@
   
 const router = require('express').Router();
-// const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
