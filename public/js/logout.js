21ae484c36aca199f8bcc821e3802f314dff1743
Dereje Ayele <derejeatomsa2@gmail.com>
2021-06-11 21:04:16 -0400

    login logout

--- public/js/logout.js
+++ public/js/logout.js
@@ -5,7 +5,7 @@ async () => {
     });
   
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/login');
     } else {
       alert(response.statusText);
     }
